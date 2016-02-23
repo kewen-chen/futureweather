339d4b4093ee1dfd3126f5a0a78e57be2330ae07
Tony <tony@gmail.com>
2016-02-23 12:35:07 +0800

    加入显示天气信息的功能。

--- src/com/futureweather/app/util/Utility.java
+++ src/com/futureweather/app/util/Utility.java
@@ -1,6 +1,18 @@
 package com.futureweather.app.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.futureweather.app.model.City;
 import com.futureweather.app.model.County;
@@ -77,6 +89,54 @@ public class Utility {
 		}
 		return false;
 	}
+	
+	/**
+	 * 解析服务器返回的JSON数据，并将解析出的数据储存到本地
+	 */
+	public static void handleWeatherResponse(Context context, String response) {
+		
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, 
+					weatherCode, temp1, temp2, weatherDesp, publishTime);
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	/**
+	 * 将服务器所有的返回信息储存到SharedPreferrnces文件中。
+	 */
+	private static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = 
+				PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+		
+		
+	}
+	
+	
+	
+	
 	
 
 	
